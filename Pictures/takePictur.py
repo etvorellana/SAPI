d1be0077cbf2439197554d8d6e267f03993e2a34
Matheus Andrade <matheus.andrade@cepedi.org.br>
2021-11-08 09:47:42 -0300

    [#33980424] - Adicionada configuração dotenv para script takePictur (sem teste)

--- Pictures/takePictur.py
+++ Pictures/takePictur.py
@@ -1,8 +1,11 @@
+import os
+from dotenv import load_dotenv, find_dotenv
 from gpiozero import Button
 from picamera import PiCamera
 from datetime import datetime
 from signal import pause
- 
+
+load_dotenv(find_dotenv())
 
 clikButton = Button(6)
 
@@ -11,6 +14,7 @@ camera = PiCamera()
 def capture():
     timestamp = datetime.now().isoformat()
     camera.capture('/home/pi/Pictures/%s.png' % timestamp)
+    camera.capture('%s%s.png' % (os.getenv("DEFAULT_PATH"), timestamp))
 
 clikButton.when_pressed = capture
 
