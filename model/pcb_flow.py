edcec66fa3dc5da798e4384aa8c0df411ebd6373
Matheus Andrade <matheus.andrade@cepedi.org.br>
2021-11-17 10:51:15 -0300

    [#33980458] - Correção na implementação dos temporizadores

--- model/pcb_flow.py
+++ model/pcb_flow.py
@@ -1,3 +1,5 @@
+from time import perf_counter
+from .temporizador import Temporizador
 class PCBFlow():
     def __init__(self, src, borda : int = 1, filtro : int = 1) -> None:
         self.img_src = src
@@ -8,6 +10,22 @@ class PCBFlow():
         self.tempos = []
         self.borda = borda
         self.filtro = filtro
+
+    def start_timer(self, method : str):
+        for tempo in self.tempos:
+            if tempo.method == method:
+                tempo.start_timer()
+                return
+        temporizador : Temporizador = Temporizador(method)
+        temporizador.start_timer()
+        self.tempos.append(temporizador)
+    
+    def stop_timer(self, method : str) -> bool:
+        for tempo in self.tempos:
+            if tempo.method == method:
+                tempo.stop_timer()
+                return 0
+        return -1
     
     def print_timers(self):
         print("------------------------")
--- view/flow.py
+++ view/flow.py
@@ -13,43 +13,35 @@ segmentacaoService : SegmentacaoService = SegmentacaoService()
 
 def executar_flow(pcb_flow : PCBFlow):
     # Detecção de bordas
-    temporizador : Temporizador = Temporizador("Corner")
-    temporizador.start_timer()
+    pcb_flow.start_timer("Corner")
 
     pcb_flow.img_bordas = deteccaoBordasService.tratar(pcb_flow)
 
-    temporizador.stop_timer()
-    pcb_flow.tempos.append(temporizador)
+    pcb_flow.stop_timer("Corner")
 
 
     # Normalização de cor
-    temporizador : Temporizador = Temporizador("Normalização")
-    temporizador.start_timer()
+    pcb_flow.start_timer("Normalização")
 
     pcb_flow.img_norm = normalizacaoService.tratar(pcb_flow)
 
-    temporizador.stop_timer()
-    pcb_flow.tempos.append(temporizador)
+    pcb_flow.stop_timer("Normalização")
 
 
     # Thresholding
-    temporizador : Temporizador = Temporizador("Thresold")
-    temporizador.start_timer()
+    pcb_flow.start_timer("Thresold")
 
     pcb_flow.thrGray = thresholdService.tratar(pcb_flow)
 
-    temporizador.stop_timer()
-    pcb_flow.tempos.append(temporizador)
+    pcb_flow.stop_timer("Thresold")
 
 
     # Segmentação
-    temporizador : Temporizador = Temporizador("Segmentação")
-    temporizador.start_timer()
+    pcb_flow.start_timer("Segmentação")
 
     img, segList = segmentacaoService.tratar(pcb_flow)
 
-    temporizador.stop_timer()
-    pcb_flow.tempos.append(temporizador)
+    pcb_flow.stop_timer("Segmentação")
     
     
     pcb_flow.print_timers()
