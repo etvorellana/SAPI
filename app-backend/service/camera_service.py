5fa52178ea22fd55843b874c35162f9f8347b05a
Jesulino Macêdo <jfelipemm10@gmail.com>
2022-02-19 03:33:08 -0300

    Endpoint para stream da camera

--- app-backend/service/camera_service.py
+++ app-backend/service/camera_service.py
@@ -1,7 +1,32 @@
+import os
+import cv2 as cv
+
 class CameraService():
     def __init__(self):
-        pass
+        if os.environ.get("FLASK_ENV") == "production":
+            self.camera = cv.VideoCapture(0)
+        else:
+            self.camera = cv.VideoCapture("./app-backend/media/video_sample.mp4")
+        self.camera.set(cv.CAP_PROP_FRAME_WIDTH, 1280)
+        # self.camera.set(cv.CAP_PROP_FRAME_HEIGHT, 720)
+        self.camera.set(cv.CAP_PROP_BUFFERSIZE, 1)
+
+    def __del__(self):
+        self.camera.release()
 
     def get_frame(self):
-        frame = ""
-        return frame
+        while True:
+            success, frame = self.camera.read()
+
+            if not success:
+                self.camera.set(cv.CAP_PROP_POS_FRAMES, 0)
+                continue
+
+            flag, encoded_frame = cv.imencode('.jpg', frame)
+
+            if not flag:
+                continue
+
+            output_frame = bytearray(encoded_frame)
+
+            yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + output_frame + b'\r\n')
