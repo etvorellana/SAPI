1313e05b86b8c3dc01e739ff1808602688e0f985
Jesulino Macêdo <jfelipemm10@gmail.com>
2022-02-21 23:29:32 -0300

    Suporte a reconexão com manutenção do último estado

--- app-backend/controller/analysis_controller.py
+++ app-backend/controller/analysis_controller.py
@@ -1,4 +1,5 @@
 import json
+import threading
 import time
 from flask import Blueprint
 from flask_sock import Sock, ConnectionClosed
@@ -7,30 +8,46 @@ from service.analysis_service import AnalysisService
 analysis_blueprint = Blueprint('analysis_blueprint', __name__)
 sock = Sock(analysis_blueprint)
 analysis_service = AnalysisService()
+analysis_thread = threading.Thread(target=analysis_service.start, args=(), daemon=True)
+analysis_thread.start()
+global connection_count
+connection_count = 0
 
 @sock.route('')
 def analysis(sock):
+    global connection_count
+    connection_count += 1
+
     try:
-        print(f'Starting analysis websocket route...')
-        analysis_service.started = True
-        analysis_service.state_service.set_state(1)
+        if analysis_service.started:
+            message_json = json.dumps(analysis_service.result)
+            print("Client reconnected...")
+            print(f"Sending message for state: {analysis_service.result['state']}")
+            sock.send(message_json)
+        else:
+            print(f'Starting analysis websocket route...')
+            analysis_service.started = True
 
         while True:
-            result = analysis_service.orchestrate_state()
-            # state 1 - start of process
-            # state 2 - watching camera
-            # state 3 - processing image start
-            # state 4 - processing image finished
-            # state 5 - showing image and waiting for restart
-            if result['state'] == 1 or result['state'] == 3 or result['state'] == 4: # only these states are sent to client
-                message_json = json.dumps(result)
-                print(f"Sending message for state: {result['state']}")
-                sock.send(message_json)
+            with analysis_service.lock:
+                result = analysis_service.result
+                # state 1 - start of process
+                # state 2 - watching camera
+                # state 3 - processing image start
+                # state 4 - processing image finished
+                # state 5 - showing image and waiting for restart
+                if result is not None and (result['state'] == 1 or result['state'] == 3 or result['state'] == 4):
+                    message_json = json.dumps(result)
+                    print(f"Sending message for state: {result['state']}")
+                    sock.send(message_json)
+
             time.sleep(0.1)
 
     except ConnectionClosed as ex:
         print(f"Connection terminated by the client. {ex}")
     except Exception as ex:
         print(f"Unexpected error occurred. {ex}")
-    
-    analysis_service.started = False
+
+    connection_count -= 1
+    if connection_count == 0:
+        analysis_service.started = False
--- app-backend/service/analysis_service.py
+++ app-backend/service/analysis_service.py
@@ -1,6 +1,7 @@
 import os
+import threading
+import time
 import imutils
-from gpiozero import Button
 from model.pcb_flow import PCBFlow
 from service.state_service import StateService
 from service.camera_service import CameraService
@@ -9,54 +10,62 @@ from util.img_util import from_np_array_to_base64
 
 class AnalysisService:
     def __init__(self):
-        self.pi_button = Button(2)
+        from controller.button_controller import pi_button
         self.button_pressed = False
-        self.pi_button.when_released = self.button_released
+        pi_button.when_released = self.button_released
         self.state_service = StateService()
         self.camera_service = CameraService()
         self.started = False
         self.frame_to_process = None
         self.image = None
+        self.result = None
+        self.lock = threading.Lock()
+
+    def start(self):
+        while True:
+            if self.started:
+                self.result = self.orchestrate_state()
+            time.sleep(0.1)
 
     def orchestrate_state(self):
-        state = self.state_service.state
-        solders_state = None
+        with self.lock:
+            state = self.state_service.state
+            solders_state = None
 
-        if self.button_pressed:
-            self.state_service.change_state()
-            self.button_pressed = False
+            if self.button_pressed:
+                self.state_service.change_state()
+                self.button_pressed = False
 
-        if state == 1:
-            self.image = None
-            self.state_service.change_state()
-        elif state == 3:
-            self.frame_to_process = self.camera_service.get_frame()
-            image_width = int(os.environ.get("DEFAULT_IMAGE_WIDTH"))
-            self.image = imutils.resize(self.frame_to_process, width=image_width)
-            self.image = from_np_array_to_base64(self.image)
-            self.state_service.change_state()
-        elif state == 4:
-            if self.frame_to_process is None:
-                print("No frame set for analysis!")
-                raise RuntimeError("Unable to acquire frame for analysis.")
-            result_image, solders_state = self.orchestrate_analysis(self.frame_to_process)
-            image_width = int(os.environ.get("DEFAULT_IMAGE_WIDTH"))
-            self.image = imutils.resize(result_image, width=image_width)
-            self.image = from_np_array_to_base64(self.image)
-            self.state_service.change_state()
-        
-        result = {"state": state, "solders_state": solders_state, "image": self.image}
-        return result
+            if state == 1:
+                self.image = None
+                self.state_service.change_state()
+            elif state == 3:
+                self.frame_to_process = self.camera_service.get_frame()
+                image_width = int(os.environ.get("DEFAULT_IMAGE_WIDTH"))
+                self.image = imutils.resize(self.frame_to_process, width=image_width)
+                self.image = from_np_array_to_base64(self.image)
+                self.state_service.change_state()
+            elif state == 4:
+                if self.frame_to_process is None:
+                    print("No frame set for analysis!")
+                    raise RuntimeError("Unable to acquire frame for analysis.")
+                result_image, solders_state = self.orchestrate_analysis(self.frame_to_process)
+                image_width = int(os.environ.get("DEFAULT_IMAGE_WIDTH"))
+                self.image = imutils.resize(result_image, width=image_width)
+                self.image = from_np_array_to_base64(self.image)
+                self.state_service.change_state()
+
+            result = {"state": state, "solders_state": solders_state, "image": self.image}
+            return result
 
     def orchestrate_analysis(self, image):
         # orchestrate analysis
         print(f"Starting image analysis...")
         pcb_flow = PCBFlow(image)
-        result_image = executar_flow(pcb_flow)
+        result_image, segList = executar_flow(pcb_flow)
         solders_state = {}
         return result_image, solders_state
 
     def button_released(self):
         print(f'Button pressed!')
-        if self.started:
-            self.button_pressed = True
+        self.button_pressed = True
