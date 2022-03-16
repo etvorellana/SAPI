09ce0fa305de77eb743be57aa937f78734c92a3d
Hiago Rios <hiagoriosc@gmail.com>
2022-03-15 23:20:06 -0300

    Recebendo classificaçao

--- frontend/src/app/core/models/message.ts
+++ frontend/src/app/core/models/message.ts
@@ -1,5 +1,19 @@
 export interface Message {
     state: number
-    solders_state: any
+    solders_classification: SoldersInfo
     image: string
-};
+}
+
+export interface SoldersInfo {
+    classificacao: SoldersClassification;
+    qtd_soldas: number;
+}
+
+export interface SoldersClassification {
+    Ausente: number
+    Boa: number
+    Excesso: number
+    Ponte: number
+    Pouca: number
+}
+
