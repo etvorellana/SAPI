64bd4e53c70d193e0de43b8592cca1e12b6412c1
Cleviton Borges <clevitonborges@gmail.com>
2022-03-24 15:50:10 -0300

    Nova função de normalização e alterações para funcionamento

--- backend/service/normalizacao_service.py
+++ backend/service/normalizacao_service.py
@@ -1,26 +1,49 @@
 from model.pcb_flow import PCBFlow
 import numpy as np
 import cv2 as cv
+import math
 
 class NormalizacaoService():
     def tratar(self, pcb_flow : PCBFlow):
-        return self.colorN(pcb_flow.img_bordas)
+        return self.normIllumination(pcb_flow.img_bordas)
+    
+    def normIllumination(self, pcb_flow : PCBFlow, DCTdistance = 10, dataType = "float64", base = 2):
+        
+        img = cv.cvtColor(pcb_flow.img_bordas, cv.COLOR_RGB2GRAY) 
 
-    def colorN(self, dstRGB, Ddis = 15):
-        h, w, c = dstRGB.shape
-        #h_ = getOptimalDCTSize(h);
-        #w_ = getOptimalDCTSize(w);
-        vis0 = np.ones((h, w, c), np.float64)
-        vis0[:h, :w, :] += dstRGB[:, :, :]
-        #vis0[h:, :, :] = 255
-        #vis0[:, w:, :] = 255
-        vis0 = np.log10(vis0)
-        for color in range(3):
-            vis0[:,:,color] = cv.dct(vis0[:,:,color])
-            for i in range(Ddis):
-                for j in range(Ddis - i):
-                    vis0[i, j, color] = 0
-            vis0[:,:,color] = cv.idct(vis0[:,:,color])
-            vis0[:,:,color] = (10.0**vis0[:,:,color]) - 1
-            dstRGB[:, :, color] = cv.normalize(vis0[:h,:w,color], None, 0, 255, cv.NORM_MINMAX, dtype=cv.CV_8U)
-        return dstRGB
+        # 1. Log transform
+        lut = np.asarray([math.log(i, base) for i in (np.arange(0, 256) + 1)], dataType)
+        img_log = cv.LUT(img, lut)
+
+        # 2. DCT transform
+        DCTCoeficient = cv.dct(img_log)
+
+        # 2.1. Remove DCT coeficients
+        if(DCTdistance > 1):
+            indices = np.array([[j, i-j] for i in np.arange(1, DCTdistance) for j in np.arange(i+1)])
+            DCTCoeficient[indices.T[0], indices.T[1]] = 0
+
+        # 3. IDCT transform
+        img_IDCT = cv.idct(DCTCoeficient)
+
+        img_IDCT_2 = img_IDCT.copy()
+
+        # 3.1. Normalizing values between [0, 1]
+        imgMax = np.max(img_IDCT)
+        imgMin = np.min(img_IDCT)
+        img_IDCT -= imgMin
+        img_IDCT /= (imgMax-imgMin)
+
+        # Option
+        # # 3.1. (b) Inverse log transform
+        height = img_IDCT_2.shape[0]
+        width = img_IDCT_2.shape[1]
+        img_IDCT_linear = np.asarray([(math.pow(base, img_IDCT_2[i,j]) - 1)
+                        for i in np.arange(height)
+                        for j in np.arange(width)]).reshape(height, width)
+        img_IDCT_linear[img_IDCT_linear < 0] = 0
+        img_IDCT_linear[img_IDCT_linear > 255] = 255
+
+        img_IDCT_linear_uint8 = (img_IDCT_linear).astype("uint8")
+
+        return img_IDCT_linear_uint8
--- backend/service/threshold_service.py
+++ backend/service/threshold_service.py
@@ -1,22 +1,21 @@
 from model.pcb_flow import PCBFlow
 import numpy as np
 from skimage.color import rgb2yiq
+import cv2 as cv
 
 class ThresholdService():
     def tratar(self, pcb_flow : PCBFlow):
         return self.threshold(pcb_flow.img_norm)
     
-    def threshold(self, dstRGB):
-        cutYIQ = rgb2yiq(dstRGB)
-        #thrGray = np.zeros_like(dstGray)
-        Y_min = cutYIQ[:, :, 0].min() # Menor pixel com cor
-        Y_max = cutYIQ[:, :, 0].max() # Maior pixel com cor
+    def threshold(self, srcNorm):
+        Y_min = int(srcNorm.min()) # Menor pixel com cor
+        Y_max = int(srcNorm.max()) # Maior pixel com cor
         Y_med = (Y_max + Y_min) / 2 #   Media das cores
         cont = 0
         con_L0 = 0
         con_L1 = 1
-        N, M, d = cutYIQ.shape
-        a = cutYIQ[:, :, 0]
+        N, M = srcNorm.shape
+        a = srcNorm
         quantT = N*M
         while ((con_L0 != con_L1) and (cont < 100)):
             cont += 1
@@ -26,15 +25,13 @@ class ThresholdService():
             quant = np.count_nonzero(b)
             con_L1 = quant
             Y_min = soma/quant
-            soma = a.sum() - soma;
+            soma = a.sum() - soma
             quant = quantT - quant
-            #print(soma, quant, soma/quant)
             Y_max = soma/quant
             Y_med = (Y_max + Y_min) / 2.0
 
-        # print(cont)
         thrGray = np.where(a < Y_med, 0, 255)
         #thrGray = cv.normalize(thrGray, None, 0, 255, cv.NORM_MINMAX, dtype=cv.CV_8U)
         thrGray = np.array(thrGray, dtype=np.uint8)
-        #print(thrGray.shape)
+        
         return thrGray #    Retorna a imagem binarizada
