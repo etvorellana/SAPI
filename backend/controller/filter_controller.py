a19a7031a5b72acefc26d3cb92e3bac92bab2d9d
Hiago Rios <hiagoriosc@gmail.com>
2022-08-16 22:18:43 -0300

    Integrando seleção de filtro

--- backend/controller/filter_controller.py
+++ backend/controller/filter_controller.py
@@ -5,7 +5,7 @@ filter_blueprint = Blueprint('filter_blueprint', __name__)
 
 @filter_blueprint.route('',  methods = ['POST'])
 def change_filter():
-    filter_service.current_filter = request.json["filter"]
+    filter_service.change_filter(request.json["filter"])
     print(f"Current filter: {filter_service.current_filter}")
     return jsonify({"success": True})
 
--- backend/service/filter_service.py
+++ backend/service/filter_service.py
@@ -1,1 +1,13 @@
+import threading
+
+global current_filter
 current_filter = 1
+global lock
+lock = threading.Lock()
+
+def change_filter(filter):
+    global lock
+    with lock:
+        global current_filter
+        current_filter = filter
+        return current_filter
