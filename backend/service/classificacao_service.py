b07b787facfab61cc2f1ee5c86d7e7e5487eb5db
Jesulino Macêdo <jfelipemm10@gmail.com>
2022-08-30 23:25:15 -0300

    Aplicando filtro dct

--- backend/service/classificacao_service.py
+++ backend/service/classificacao_service.py
@@ -1,6 +1,4 @@
-import math
 import numpy as np
-import cv2 as cv
 from csv import reader
 from service.filtros.dct_service import DctService
 from service.filtros.log_gabor_service import LogGaborService
@@ -9,49 +7,57 @@ import model.constantes as constantes
 
 class ClassificacaoService():
     def __init__(self):
-        self.lista_base = ClassificacaoService.obter_base_dados()
+        self.lista_base_gabor, self.lista_base_dct = ClassificacaoService.obter_base_dados()
 
     def classificar(self, pcb_flow : PCBFlow, segment):
         lista_valores = []
 
-        print("Aplicando filtro...")
         if pcb_flow.filtro == 1:
             cropped_image = pcb_flow.img_bordas[segment[3]:segment[3]+segment[5], segment[2]:segment[2]+segment[4]]
             gaborService = LogGaborService()
             lista_valores = gaborService.gaborSum(cropped_image)
+            classificacao = ClassificacaoService.calculo(lista_valores, self.lista_base_gabor[0:200], self.lista_base_gabor[200:400], self.lista_base_gabor[400:600], self.lista_base_gabor[600:800], self.lista_base_gabor[800:1000])
         elif pcb_flow.filtro == 2:
             dctService = DctService()
             dct_filtered_img = dctService.dct_filter(pcb_flow.img_bordas)
             cropped_image = dct_filtered_img[segment[3]:segment[3]+segment[5], segment[2]:segment[2]+segment[4]]
             lista_valores = dctService.dctSum(cropped_image)
+            classificacao = ClassificacaoService.calculo(lista_valores, self.lista_base_dct[0:200], self.lista_base_dct[200:400], self.lista_base_dct[400:600], self.lista_base_dct[600:800], self.lista_base_dct[800:1000])
         elif pcb_flow.filtro == 3:
             pass
         else:
             print("Filtro inválido!")
             return
 
-        print("Classificando...")
-
-        classificacao = ClassificacaoService.calculo(lista_valores, self.lista_base[0:200], self.lista_base[200:400], self.lista_base[400:600], self.lista_base[600:800], self.lista_base[800:1000])
-
         return classificacao
 
     def obter_base_dados():
-        lista_soldas = []
+        lista_soldas_gabor = []
+        lista_soldas_dct = []
 
         with open('media/Gabor_1k.csv', 'r') as csv_file:
             csv_reader = reader(csv_file)
-            lista_soldas = list(csv_reader)
+            lista_soldas_gabor = list(csv_reader)
 
-        del lista_soldas[0]
+        with open('media/Dct.csv', 'r') as csv_file:
+            csv_reader = reader(csv_file)
+            lista_soldas_dct = list(csv_reader)
 
-        print("Tamanho do conjunto de treinamento: ", len(lista_soldas))
+        del lista_soldas_gabor[0]
+        del lista_soldas_dct[0]
 
-        tam = len(lista_soldas)
+        print("Tamanho do conjunto de treinamento gabor: ", len(lista_soldas_gabor))
+        print("Tamanho do conjunto de treinamento dct: ", len(lista_soldas_dct))
+
+        tam = len(lista_soldas_gabor)
         for x in range(tam):
-            lista_soldas[x] = list(map(float, lista_soldas[x]))
+            lista_soldas_gabor[x] = list(map(float, lista_soldas_gabor[x]))
 
-        return lista_soldas
+        tam = len(lista_soldas_dct)
+        for x in range(tam):
+            lista_soldas_dct[x] = list(map(float, lista_soldas_dct[x]))
+
+        return lista_soldas_gabor, lista_soldas_dct
 
     def calculo(l_teste, l_treinamento_boa, l_treinamento_pouca, l_treinamento_ponte, l_treinamento_excesso, l_treinamento_ausente):
         classificacao = []
